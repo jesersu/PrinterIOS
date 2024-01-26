be02c2468a3b9538f9954edab0f3079dba3da5a4
jesersu <jesersu@gmail.com>
2024-01-26 14:20:14 -0500

    ADD THREE COLLUMNS

--- Sources/Printer/Receipt/Text.swift
+++ Sources/Printer/Receipt/Text.swift
@@ -48,6 +48,46 @@ public struct KVItem: ReceiptItem {
     }
 }
 
+// MARK: ADD THREE COLUMNS
+public struct XYZItem: ReceiptItem {
+    public let x: String
+    public let y: String
+    public let z: String
+    
+    public init(_ x: String, _ y: String, _ z: String) {
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+    
+    public func assemblePrintableData(_ profile: PrinterProfile) -> [UInt8] {
+        var num = profile.maxWidthDensity / profile.fontDensity
+        
+        let string = x + y + z
+        
+        for c in string {
+            if (c >= "\u{2E80}" && c <= "\u{FE4F}") || c == "\u{FFE5}" {
+                num -= 2
+            } else {
+                num -= 1
+            }
+        }
+        
+        let spaceInRight = num/2
+        let spaceInLeft = num - spaceInRight
+        
+        var contentsLeft = stride(from: 0, to: spaceInLeft, by: 1).map { _ in " " }
+        let contentsRight = stride(from: 0, to: spaceInRight, by: 1).map { _ in " " }
+        
+        contentsLeft.insert(x, at: 0)
+        contentsLeft.append(y)
+        contentsLeft.append(contentsOf: contentsRight)
+        contentsLeft.append(z)
+        
+        return contentsLeft.joined().assemblePrintableData(profile)
+    }
+}
+
 public protocol DividingProvider {
     func character(for current: Int, total: Int) -> Character
 }
