858dcdda954988adbfe9ebcca171dd547abc5ac6
jesersu <jesersu@gmail.com>
2024-07-17 04:22:44 -0500

    Add custom cell

---
+++ Sources/Printer/Hardware/Bluetooth/cell/BluetoothDeviceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BluetoothDeviceTableViewCell.swift
+//  Pods
+//
+//  Created by Jesus Ervin Chapi Suyo on 17/07/24.
+//
+
+import UIKit
+
+class BluetoothDeviceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
